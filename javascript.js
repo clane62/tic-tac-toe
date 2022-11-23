bc07faa73ab62dd6214b22fa0ba6403d1ff5d055
clane <lane.caitlyn@gmail.com>
2022-11-23 15:10:32 +1100

    updated project in progress

--- javascript.js
+++ javascript.js
@@ -1,70 +1,84 @@
-{/* <section class="tic-tac-toe">
-
-// edi tng for testing git
-
-<div class="box"></div>   0 - top left
-<div class="box"></div>   1 - top middle
-<div class="box"></div>   2 - top right     
-<div class="box"></div>   3 - middle left
-<div class="box"></div>   4 - middle middle
-<div class="box"></div>   5 - middle right
-<div class="box"></div>   6 - bottom left
-<div class="box"></div>   7 - bottom middle
-<div class="box"></div>   8 - bottom right */}
-
-
 var ticTacGrid = document.querySelectorAll('.box')
-
-// ticTacGrid.forEach(function(clickBox) {
-//     clickBox.addEventListener('click', function (event){ 
-      
-//         let clickedOnBox = event.target
-//         clickedOnBox.style.background = 'blue'
-//     })
-// })
-
-// above code works. Keeping here for safe keeping even though the code below is the same. Each box, when clicked is colored blue. 
-
+var playerOneTurn = true
 
 ticTacGrid.forEach(function(clickBox) {
-    clickBox.addEventListener('click', function (event){ 
-      
-        let clickedOnBox = event.target
-        clickedOnBox.style.background = 'blue'
-        clickedOnBox.className = '.blue-box'
-
-        if ((ticTacGrid[0].style.background  === 'blue') && (ticTacGrid[1].style.background  === 'blue') && (ticTacGrid[2].style.background  === 'blue')) {
-            console.log('winner012')
-
-        } else if ((ticTacGrid[3].style.background  === 'blue') && (ticTacGrid[4].style.background  === 'blue') && (ticTacGrid[5].style.background  === 'blue')) {
-            console.log('winner345')
-
-        } else if ((ticTacGrid[6].style.background  === 'blue') && (ticTacGrid[7].style.background  === 'blue') && (ticTacGrid[8].style.background  === 'blue')) {
-            console.log('winner678') 
-
-        } else if ((ticTacGrid[0].style.background  === 'blue') && (ticTacGrid[3].style.background  === 'blue') && (ticTacGrid[6].style.background  === 'blue')) {
-            console.log('winner036') 
-
-        } else if ((ticTacGrid[1].style.background  === 'blue') && (ticTacGrid[4].style.background  === 'blue') && (ticTacGrid[7].style.background  === 'blue')) {
-            console.log('winner147') 
-
-        } else if ((ticTacGrid[2].style.background  === 'blue') && (ticTacGrid[5].style.background  === 'blue') && (ticTacGrid[8].style.background  === 'blue')) {
-            console.log('winner258') 
+    clickBox.addEventListener('click', function (event) { 
         
-        } else if ((ticTacGrid[0].style.background  === 'blue') && (ticTacGrid[4].style.background  === 'blue') && (ticTacGrid[8].style.background  === 'blue')) {
-            console.log('winner048') 
+        let clickedOnBox = event.target
 
-        } else if ((ticTacGrid[2].style.background  === 'blue') && (ticTacGrid[4].style.background  === 'blue') && (ticTacGrid[6].style.background  === 'blue')) {
-            console.log('winner246') 
+        if ((playerOneTurn === false) && ((clickedOnBox.style.background === 'white') || (clickedOnBox.style.background === ''))) {
+            (clickedOnBox.style.background = 'pink') && (clickedOnBox.className = '.player-two')
+            playerOneWin()
+            playerTwoWin()
+            playerOneTurn = true
+
+        } else if ((playerOneTurn === true) && ((clickedOnBox.style.background === 'white') || (clickedOnBox.style.background === ''))) {
+            (clickedOnBox.style.background = 'purple') && (clickedOnBox.className = '.player-one') 
+                playerOneWin()
+                playerTwoWin()
+                playerOneTurn = false   
+        
+        }  else if ((clickedOnBox.style.background == 'purple') || (clickedOnBox.style.background == 'pink')) {
+            console.log('Sorry that box is clicked, choose another')
         }
+        })
     })
-})
-
-// next steps:
-// change class name to identify items already clicked? (can this be two different uses?)
-// click is update name. 
-// toggle between two 'people' clicks? Google how to differentiate alternate clicks. 1,2 1,2 etc. toggle()?
-// work out how to announce winner (and end the game)
-// add a draw. 
 
 
+
+function playerOneWin() {
+    if ((ticTacGrid[0].className === '.player-one') && (ticTacGrid[1].className === '.player-one') && (ticTacGrid[2].className === '.player-one')) {
+        console.log('winner one 012')
+    }
+    if ((ticTacGrid[3].className === '.player-one') && (ticTacGrid[4].className === '.player-one') && (ticTacGrid[5].className === '.player-one')) {
+        console.log('winner one 345')
+    }
+    if ((ticTacGrid[6].className === '.player-one') && (ticTacGrid[7].className === '.player-one') && (ticTacGrid[8].className === '.player-one')) {
+        console.log('winner one 678')
+    }
+    if ((ticTacGrid[0].className === '.player-one') && (ticTacGrid[3].className === '.player-one') && (ticTacGrid[6].className === '.player-one')) {
+        console.log('winner one 036') 
+    }
+    if ((ticTacGrid[1].className === '.player-one') && (ticTacGrid[4].className === '.player-one') && (ticTacGrid[7].className === '.player-one')) {
+        console.log('winner one 147') 
+    }
+    if ((ticTacGrid[2].className === '.player-one') && (ticTacGrid[5].className === '.player-one') && (ticTacGrid[8].className === '.player-one')) {
+        console.log('winner one 258') 
+    } 
+    if ((ticTacGrid[0].className === '.player-one') && (ticTacGrid[4].className === '.player-one') && (ticTacGrid[8].className === '.player-one')) {
+        console.log('winner one 048')
+    }   
+    if ((ticTacGrid[2].className === '.player-one') && (ticTacGrid[4].className === '.player-one') && (ticTacGrid[6].className === '.player-one')) {
+        console.log('winner one 246')
+    }
+
+}
+
+
+function playerTwoWin() {
+
+if ((ticTacGrid[0].className === '.player-two') && (ticTacGrid[1].className === '.player-two') && (ticTacGrid[2].className === '.player-two')) {
+    console.log('winner two 012')
+} 
+if ((ticTacGrid[3].className === '.player-two') && (ticTacGrid[4].className === '.player-two') && (ticTacGrid[5].className === '.player-two')) {
+    console.log('winner two 345')
+} 
+if ((ticTacGrid[6].className === '.player-two') && (ticTacGrid[7].className === '.player-two') && (ticTacGrid[8].className === '.player-two')) {
+    console.log('winner two 678') 
+} 
+if ((ticTacGrid[0].className === '.player-two') && (ticTacGrid[3].className === '.player-two') && (ticTacGrid[6].className === '.player-two')) {
+    console.log('winner two 036') 
+} 
+if ((ticTacGrid[1].className === '.player-two') && (ticTacGrid[4].className === '.player-two') && (ticTacGrid[7].className === '.player-two')) {
+    console.log('winner two 147') 
+} 
+if ((ticTacGrid[2].className === '.player-two') && (ticTacGrid[5].className === '.player-two') && (ticTacGrid[8].className === '.player-two')) {
+    console.log('winner two 258') 
+} 
+if ((ticTacGrid[0].className === '.player-two') && (ticTacGrid[4].className === '.player-two') && (ticTacGrid[8].className === '.player-two')) {
+    console.log('winner two 048')
+} 
+if ((ticTacGrid[2].className === '.player-two') && (ticTacGrid[4].className === '.player-two') && (ticTacGrid[6].className === '.player-two')) {
+    console.log('winner two 246') 
+}
+}
